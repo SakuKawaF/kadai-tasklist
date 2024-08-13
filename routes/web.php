1d3487739512244a290f06e032b5ca0d8f8dd0ba
yuu.k <sakusakuyf@gmail.com>
2024-08-13 06:23:21 +0000

    CRUD tasks

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TasksController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [TasksController::class, 'index']);
+Route::resource('tasks', TasksController::class);
+
+Route::get('tasks/{id}', [TasksController::class, 'show']);
+Route::post('tasks/ceate', [TasksController::class, 'store']);
+Route::put('tasks/{id}', [TasksController::class, 'update']);
+Route::delete('tasks/{id}', [TasksController::class, 'destroy']);
+
+Route::get('tasks', [TasksController::class, 'index'])->name('tasks.index');
+Route::get('tasks/create', [TasksController::class, 'create'])->name('tasks.create');
+Route::get('tasks/{id}/edit', [TasksController::class, 'edit'])->name('tasks.edit');
